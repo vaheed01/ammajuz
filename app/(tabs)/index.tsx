96f7ee13e1648ae7dc44cb013eaabfe1e94aa43a
Vahid Rafi-E <vaheedafter@gmail.com>
2025-11-14 18:27:25 +0330

    Hello World! I'm gonna rewrite my flutter project using react native :)

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+	  Hello World!
+	  I'm gonna rewrite my flutter project using react native :)
+	  </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  text: { fontSize: 24, fontWeight: 'bold' }
+});
